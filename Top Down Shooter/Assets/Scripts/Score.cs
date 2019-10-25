d88bc33a34e945b0ac93c7dfde37c26fbe6d57fb
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2019-10-25 22:59:46 +0300

    increasing score

---
+++ Top Down Shooter/Assets/Scripts/Score.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    Text scoreText;
+    float currentScore = 0f;
+
+    private void Awake()
+    {
+        scoreText = GetComponent<Text>();
+    }
+
+    private void ChangeScore(float points)
+    {
+        currentScore += points;
+        scoreText.text = string.Format("Score  {0}", currentScore);
+    }
+}
