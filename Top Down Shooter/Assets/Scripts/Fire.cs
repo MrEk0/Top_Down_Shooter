ca7306e0821a3db87ce917a9deb623e165f9da7f
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2019-10-25 22:59:24 +0300

    fire

---
+++ Top Down Shooter/Assets/Scripts/Fire.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    [SerializeField] GameObject buletPrefab = null;
+    [SerializeField] Transform firePoint = null;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Shot();
+        }
+    }
+
+    private void Shot()
+    {
+        GameObject bullet=Instantiate(buletPrefab, firePoint.position, firePoint.rotation);
+        bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.up * 10, ForceMode2D.Impulse);
+    }
+}
---
+++ Top Down Shooter/Assets/Scripts/Target.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Target : MonoBehaviour
+{
+    [SerializeField] float awardForDestroying = 10f;
+
+    public event Action<GameObject> OnRemoved;
+    public event Action<float> OnDestroyed;//!!!!
+
+    private void OnDestroy()
+    {
+        OnRemoved(gameObject);
+        OnDestroyed(awardForDestroying);
+    }
+
+    //public float GetAward()
+    //{
+    //    return awardForDestroying;
+    //}
+}
