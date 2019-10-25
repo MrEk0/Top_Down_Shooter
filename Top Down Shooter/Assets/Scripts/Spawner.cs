ad40b571d446a7b50cac51cff70c7028f30adb2b
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2019-10-25 22:59:02 +0300

    spawner targets

---
+++ Top Down Shooter/Assets/Scripts/Spawner.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] GameObject targetPrefab = null;
+    [SerializeField] float spawnTime = 2f;
+    [SerializeField] float offset = 1f;
+    [SerializeField] float maxTargetNumber = 10f;
+
+    float timeSinceLastSpawn = 0f;
+    float minPosX;
+    float maxPosX;
+    float minPosY;
+    float maxPosY;
+
+    float targetCount = 0f;
+    float radius;
+
+    GameManager gameManager;
+
+    private void Awake()
+    {
+        radius = targetPrefab.GetComponent<CircleCollider2D>().radius;
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    private void Start()
+    {
+        minPosX = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + offset;
+        maxPosX = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - offset;
+        minPosY = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + offset;
+        maxPosY = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - offset;
+    }
+
+    private void Update()
+    {
+        if(timeSinceLastSpawn>spawnTime)
+        {
+            Spawn();
+            timeSinceLastSpawn = 0f;
+        }
+        timeSinceLastSpawn += Time.deltaTime;
+    }
+
+    private void Spawn()
+    {
+        float spawnPosX = Random.Range(minPosX, maxPosX);
+        float spawnPosY = Random.Range(minPosY, maxPosY);
+        Vector3 startPos = new Vector3(spawnPosX, spawnPosY, 0);
+
+        if (Physics2D.CircleCast(startPos, radius, Vector2.zero))
+        {
+            return;//!!!!
+        }
+
+        if (gameManager.GetTargetNumber() < maxTargetNumber)
+        {
+            GameObject target = Instantiate(targetPrefab, startPos, Quaternion.identity, transform);
+            gameManager.AddTarget(target);
+            //target.GetComponent<Target>().OnDestroyed += ReduceTargetCount;
+            targetCount++;
+        }
+    }
+
+    //private void ReduceTargetCount()
+    //{
+    //    targetCount--;
+    //}
+
+}
